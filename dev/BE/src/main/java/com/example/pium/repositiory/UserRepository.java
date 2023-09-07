dd94b76eef5639d0ec849d6d26d9627b8a8fdc90
이승준 <ja132824@naver.com>
2023-09-07 16:43:27 +0900

    ãADD / 아이디 중복확인 / 중 중복 확인 기능 추가

--- dev/BE/src/main/java/com/example/pium/repositiory/UserRepository.java
+++ dev/BE/src/main/java/com/example/pium/repositiory/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.pium.repositiory;
 import com.example.pium.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUserId(String userId);
 }
