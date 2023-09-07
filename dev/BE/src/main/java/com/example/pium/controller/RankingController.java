ef274fc560e97924ae669868d0249bf19a81cacb
이승준 <ja132824@naver.com>
2023-09-07 17:41:41 +0900

    ADD / 랭킹 왕 조회 / 조회 가능 한 DTO 및 기능 추가

---
+++ dev/BE/src/main/java/com/example/pium/controller/RankingController.java
@@ -0,0 +1,29 @@
+package com.example.pium.controller;
+
+import com.example.pium.dto.RankingDto;
+import com.example.pium.entity.RankingEntity;
+import com.example.pium.service.RankingServiceImp;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RequiredArgsConstructor
+@RequestMapping("/ranking")
+@RestController
+@Slf4j
+public class RankingController {
+    private final RankingServiceImp rankingService;
+    @GetMapping
+    public ResponseEntity<List<RankingDto>> getRankingList() {
+        List<RankingDto> rankingList = rankingService.makeDto();
+        return ResponseEntity.ok(rankingList);
+    }
+
+}
