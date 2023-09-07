fbc829010bc45276269f8a4e39dd6d3665bc7041
seungwookim <osabero@naver.com>
2023-09-07 12:50:59 +0900

    ADD/.eslintrc.cjs수정/린트,티어설정

--- dev/FE/src/App.test.tsx
+++ dev/FE/src/App.test.tsx
@@ -1,9 +1,9 @@
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import App from "./App";
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
 
-describe("App 컴포넌트", () => {
-  test("App 컴포넌트 렌더링 테스트", () => {
+describe('App 컴포넌트', () => {
+  test('App 컴포넌트 렌더링 테스트', () => {
     render(<App />);
   });
 });
--- dev/FE/src/main.tsx
+++ dev/FE/src/main.tsx
@@ -1,19 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-import { RecoilRoot } from "recoil";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </RecoilRoot>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
