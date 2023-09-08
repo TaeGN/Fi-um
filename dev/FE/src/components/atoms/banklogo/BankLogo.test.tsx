b02bf85b56275881db2547e3e9c867284e3a1bbe
seungwookim <osabero@naver.com>
2023-09-08 14:29:13 +0900

    ADD/BankLogoComponent/BankLogoComponent 추가

--- dev/FE/src/components/atoms/banklogo/BankLogo.test.tsx
+++ dev/FE/src/components/atoms/banklogo/BankLogo.test.tsx
@@ -1,1 +1,11 @@
 // BankLogo.test.tsx
+
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import BankLogo from './BankLogo';
+
+describe('BankLogo 컴포넌트', () => {
+  test('BankLogo 컴포넌트 렌더링 테스트', () => {
+    render(<BankLogo className="shinhan" />);
+  });
+});
--- dev/FE/src/components/atoms/banklogo/BankLogo.tsx
+++ dev/FE/src/components/atoms/banklogo/BankLogo.tsx
@@ -1,5 +1,16 @@
-const BankLogo = () => {
-  return <div>BankLogo</div>;
+import { convertClassName } from '@/utils';
+import styles from '@/components/atoms/banklogo/BankLogo.module.scss';
+
+interface BankLogoProps {
+  className: string;
+}
+
+const BankLogo = ({ className }: BankLogoProps) => {
+  return (
+    <>
+      <div className={convertClassName(className, styles)}></div>
+    </>
+  );
 };
 
 export default BankLogo;
