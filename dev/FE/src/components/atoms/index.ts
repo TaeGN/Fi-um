73a2654e6d97ed653f4fab0070b0214a2d5007bf
BTDnoBacon <osabero@naver.com>
2023-09-10 15:23:14 +0900

    MOD/index.ts/atoms index.ts 수정

--- dev/FE/src/components/atoms/index.ts
+++ dev/FE/src/components/atoms/index.ts
@@ -3,3 +3,6 @@ export { default as PieChart } from '@/components/atoms/PieChart/PieChart';
 export { default as LineChart } from '@/components/atoms/LineChart/LineChart';
 export { default as BarChart } from '@/components/atoms/BarChart/BarChart';
 export { default as Text } from '@/components/atoms/Text/Text';
+export { default as BankLogo } from '@/components/atoms/BankLogo/BankLogo';
+export { default as Button } from '@/components/atoms/Button/Button';
+export { default as Logo } from '@/components/atoms/Logo/Logo';
