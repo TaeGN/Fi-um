d9cc345136a7a3297921fc0048ece4f800e1a40a
BTDnoBacon <osabero@naver.com>
2023-09-10 15:20:25 +0900

    MOD/molecules index.ts/molecule folder index.ts 수정

--- dev/FE/src/components/molecules/index.ts
+++ dev/FE/src/components/molecules/index.ts
@@ -1,5 +1,8 @@
 // cards folder
 export { default as FundingCard } from '@/components/molecules/cards/FundingCard/FundingCard';
+export { default as AuctionCard } from '@/components/molecules/cards/AuctionCard/AuctionCard';
+export { default as ProfileCard } from '@/components/molecules/cards/ProfileCard/ProfileCard';
+export { default as RankingCard } from '@/components/molecules/cards/RankingCard/RankingCard';
 
 // descriptions folder
 export { default as AuctionDescription } from '@/components/molecules/descriptions/AuctionDescription/AuctionDescription';
@@ -10,6 +13,14 @@ export { default as MainAuctionDescription } from '@/components/molecules/descri
 // funding folder
 export { default as FundingBar } from '@/components/molecules/funding/FundingBar/FundingBar';
 export { default as FundingItemStatus } from '@/components/molecules/funding/FundingItemStatus/FundingItemStatus';
+
+// profile folder
+export { default as ProfileAsset } from '@/components/molecules/profile/ProfileAsset/ProfileAsset';
+export { default as ProfileDonator } from '@/components/molecules/profile/ProfileDonator/ProfileDonator';
+export { default as ProfileAdmin } from '@/components/molecules/profile/ProfileAdmin/ProfileAdmin';
+
+// stock folder
+export { default as StockRanking } from '@/components/molecules/stock/StockRanking/StockRanking';
 
 // utils folder
 export { default as Carousel } from '@/components/molecules/utils/Carousel/Carousel';
