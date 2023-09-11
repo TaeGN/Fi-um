806f32a3b43b50961d8325f0936de3f8a5f6f271
seungwookim <osabero@naver.com>
2023-09-11 11:23:13 +0900

    MOD/ProfileCard/ProfileCard 설계

--- dev/FE/src/components/molecules/cards/ProfileCard/ProfileCard.tsx
+++ dev/FE/src/components/molecules/cards/ProfileCard/ProfileCard.tsx
@@ -1,5 +1,41 @@
-const ProfileCard = () => {
-  return <div>ProfileCard</div>;
+import { Button, Image, Text } from '@/components/atoms';
+import { convertClassName } from '@/utils';
+import styles from './ProfileCard.module.scss';
+
+interface ProfileCardProps {
+  src: string;
+  alt: string;
+  text: string;
+  className?: string;
+  onClick?: () => void;
+  userCheck?: boolean;
+}
+
+const ProfileCard = ({
+  src,
+  alt,
+  text,
+  className,
+  onClick,
+  userCheck,
+}: ProfileCardProps) => {
+  return (
+    <div
+      className={`${styles.profileCard} ${convertClassName(className, styles)}`}
+    >
+      <div className={styles.image}>
+        <Image src={src} alt={alt} />
+      </div>
+      <div className={styles.text}>
+        <Text text={text} className="text-lg" />
+      </div>
+      {userCheck && (
+        <div className={styles.button}>
+          <Button label="정보 수정" className="gray xsmall" onClick={onClick} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ProfileCard;
