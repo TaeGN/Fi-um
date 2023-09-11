8918a8021c3d0c966b00e234cdbadc1fe7b4b40e
taegn <taegyunn123@gmail.com>
2023-09-11 11:39:15 +0900

    :black_nib: MOD / MainPage / MainPage 수정

--- dev/FE/src/pages/MainPage/MainPage.tsx
+++ dev/FE/src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,23 @@
+import styles from './MainPage.module.scss';
+import { Image, LineChart, PieChart, Table, Text } from '@/components/atoms';
+import { FundingCard } from '@/components/molecules';
+
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div className={styles['main-page']}>
+      <Image className="bg-blue big aa gg hbig big aa blue" src="" alt="aaa" />
+      <div className="card-container">
+        <FundingCard />
+        <FundingCard />
+        <PieChart />
+        <LineChart />
+        <LineChart />
+      </div>
+      <div className="mint">Hi my name is seung woo</div>
+      <Text className="text-lg blue" text="개쩌는 테이블" />
+      <Table />
+    </div>
+  );
 };
 
 export default MainPage;
