506ca8a94c0ccc97e90ff06dbaeb3917dc186702
seungwookim <osabero@naver.com>
2023-09-11 17:57:05 +0900

    MOD/rankingcard/rankingcard 코드 수정

--- dev/FE/src/components/molecules/cards/RankingCard/RankingCard.tsx
+++ dev/FE/src/components/molecules/cards/RankingCard/RankingCard.tsx
@@ -6,21 +6,34 @@ interface RankingCardProps {
   no1: string;
   no2: string;
   no3: string;
-  className?: string;
+  cardClassName?: string;
+  titleClassName?: string;
+  contentClassName?: string;
 }
 
-const RankingCard = ({ no1, no2, no3, className }: RankingCardProps) => {
+const RankingCard = ({
+  no1,
+  no2,
+  no3,
+  cardClassName,
+  titleClassName,
+  contentClassName,
+}: RankingCardProps) => {
+  const contentText = [`1등 : ${no1}`, `2등 : ${no2}`, `3등 : ${no3}`];
   return (
     <div
-      className={`${styles.rankingCard} ${convertClassName(className, styles)}`}
+      className={`${styles.rankingCard} ${convertClassName(
+        cardClassName,
+        styles,
+      )}`}
     >
       <div className={styles.title}>
-        <Text text="랭킹" className="text-xl" />
+        <Text text="랭킹" className={`text-xl ${titleClassName}`} />
       </div>
       <div className={styles.content}>
-        <Text text={`1등 : ${no1}`} />
-        <Text text={`2등 : ${no2}`} />
-        <Text text={`3등 : ${no3}`} />
+        {contentText.map((item) => {
+          return <Text key={item} text={item} className={contentClassName} />;
+        })}
       </div>
     </div>
   );
