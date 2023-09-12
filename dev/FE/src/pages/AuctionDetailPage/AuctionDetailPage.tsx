c2a7edbdfb052fdc10e8e0895bdda39f9a3e1f8e
seungwookim <osabero@naver.com>
2023-09-12 13:02:52 +0900

    MOD/auctiondetailmain/AuctionDetailMain 설계 및 auction description 수정

--- dev/FE/src/pages/AuctionDetailPage/AuctionDetailPage.tsx
+++ dev/FE/src/pages/AuctionDetailPage/AuctionDetailPage.tsx
@@ -1,7 +1,27 @@
 import './AuctionDetailpage.module.scss';
+import { AuctionDetailMain } from '@/components/organisms';
 
 const AuctionDetailPage = () => {
-  return <div>AuctionDetailPage</div>;
+  return (
+    <>
+      <div>AuctionDetailPage</div>
+      <div>
+        <AuctionDetailMain
+          src=""
+          alt=""
+          title="확인용"
+          auctionClick={() => {
+            console.log('경매하기');
+          }}
+          buyItClick={() => {
+            console.log('즉시구매');
+          }}
+          auctionPrice="124123412"
+          instantPrice="235213341"
+        />
+      </div>
+    </>
+  );
 };
 
 export default AuctionDetailPage;
