8b772bce926567965ae5b3e56daf3880354a8c2b
seungwookim <osabero@naver.com>
2023-09-12 15:21:17 +0900

    확인

--- dev/FE/src/pages/AuctionDetailPage/AuctionDetailPage.tsx
+++ dev/FE/src/pages/AuctionDetailPage/AuctionDetailPage.tsx
@@ -1,43 +1,38 @@
-import { convertClassName, convertClassNameList, loremData } from '@/utils';
-import styles from './AuctionDetailPage.module.scss';
-import { CreaterProfile } from '@/components/organisms';
-import { Image } from '@/components/atoms';
+// import { convertClassName, convertClassNameList, loremData } from '@/utils';
+// import styles from './AuctionDetailPage.module.scss';
+// import { CreaterProfile } from '@/components/organisms';
+// import { Image } from '@/components/atoms';
 
-interface AuctionDetailPageProps {
-  className?: string;
-}
-
-const AuctionDetailPage = ({
-  className,
-}: AuctionDetailPageProps): JSX.Element => {
+const AuctionDetailPage = (): JSX.Element => {
   return (
-    <div
-      className={convertClassNameList(
-        convertClassName(className, styles),
-        styles['auction-detail-page'],
-      )}
-    >
-      <Image
-        src="/vite.svg"
-        alt="image"
-        className={convertClassNameList(styles['auction-detail-page__image'])}
-      />
-      <div
-        className={convertClassNameList(styles['auction-detail-page__card'])}
-      >
-        card
-      </div>
-      <div
-        className={convertClassNameList(
-          styles['auction-detail-page__description'],
-        )}
-      >
-        {loremData}
-      </div>
-      <CreaterProfile
-        className={convertClassNameList(styles['auction-detail-page__profile'])}
-      />
-    </div>
+    <div>와</div>
+    // <div
+    //   className={convertClassNameList(
+    //     convertClassName(className, styles),
+    //     styles['auction-detail-page'],
+    //   )}
+    // >
+    //   <Image
+    //     src="/vite.svg"
+    //     alt="image"
+    //     className={convertClassNameList(styles['auction-detail-page__image'])}
+    //   />
+    //   <div
+    //     className={convertClassNameList(styles['auction-detail-page__card'])}
+    //   >
+    //     card
+    //   </div>
+    //   <div
+    //     className={convertClassNameList(
+    //       styles['auction-detail-page__description'],
+    //     )}
+    //   >
+    //     {loremData}
+    //   </div>
+    //   <CreaterProfile
+    //     className={convertClassNameList(styles['auction-detail-page__profile'])}
+    //   />
+    // </div>
   );
 };
 
