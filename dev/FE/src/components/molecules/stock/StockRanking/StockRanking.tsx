ed425a88c1b751c63c8880cc9e006d1100c971a2
seungwookim <osabero@naver.com>
2023-09-12 09:45:06 +0900

    MOD/stockranking/stockranking 설계

--- dev/FE/src/components/molecules/stock/StockRanking/StockRanking.tsx
+++ dev/FE/src/components/molecules/stock/StockRanking/StockRanking.tsx
@@ -1,5 +1,33 @@
-const StockRanking = () => {
-  return <div>StockRanking</div>;
+import { PieChart, Text } from '@/components/atoms';
+import styles from './StockRanking.module.scss';
+import { convertClassName } from '@/utils';
+
+interface StockRankingProps {
+  title: string;
+  className?: string;
+  titleClassName?: string;
+}
+
+const StockRanking = ({
+  title,
+  className,
+  titleClassName,
+}: StockRankingProps) => {
+  return (
+    <div
+      className={`${styles.stockRanking} ${convertClassName(
+        className,
+        styles,
+      )}`}
+    >
+      <div className={styles.title}>
+        <Text text={title} className={titleClassName} />
+      </div>
+      <div className={styles.chart}>
+        <PieChart />
+      </div>
+    </div>
+  );
 };
 
 export default StockRanking;
