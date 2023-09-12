e2e712048a4dffc0fd721f4c1d61705f5c785a63
taegn <taegyunn123@gmail.com>
2023-09-12 14:24:30 +0900

    :black_nib: MOD / Swiper / Swiper package 추가 & Swiper 구현

--- dev/FE/src/components/molecules/utils/Swiper/Swiper.tsx
+++ dev/FE/src/components/molecules/utils/Swiper/Swiper.tsx
@@ -1,9 +1,45 @@
-import styles from './Swiper.module.scss'
-  
-const Swiper = () => {
-  return (
-    <div>Swiper</div>
-  )
+import { convertClassName, convertClassNameList } from '@/utils';
+import styles from './Swiper.module.scss';
+import { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+interface SwiperProps {
+  className?: string;
+  children?: JSX.Element[];
 }
 
-export default Swiper;
+// Import Swiper styles
+
+const CustomSwiper = ({ className, children }: SwiperProps): JSX.Element => {
+  return (
+    <Swiper
+      className={convertClassNameList(
+        convertClassName(className, styles),
+        styles.swiper,
+      )}
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+    >
+      {children?.map((child) => (
+        <SwiperSlide className={convertClassNameList(styles['swiper-slide'])}>
+          {child}
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default CustomSwiper;
