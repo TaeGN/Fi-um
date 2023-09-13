6b657cd3bccf6362d279c979f571e47d8c9ceaa2
seungwookim <osabero@naver.com>
2023-09-13 15:10:39 +0900

    ADD/Ranking moulecule/Ranking Component 추가, dummy.jpg 추가

--- dev/FE/src/components/organisms/ranking/Ranking/Ranking.tsx
+++ dev/FE/src/components/organisms/ranking/Ranking/Ranking.tsx
@@ -1,5 +1,29 @@
+import { ProfileCard } from '@/components/molecules';
+import styles from './Ranking.module.scss';
+import { Image } from '@/components/atoms';
+
 const Ranking = () => {
-  return <div>Ranking</div>;
+  const dummy = [
+    { id: 1, name: '1번사람', src: '/img/dummy2.jpg' },
+    { id: 2, name: '2번사람', src: '/img/dummy2.jpg' },
+    { id: 3, name: '3번사람', src: '/img/dummy2.jpg' },
+  ];
+  return (
+    <div className={styles.ranking}>
+      {dummy.map((item) => {
+        return (
+          <div className={styles[`profileCard${item.id}`]}>
+            {item.id === 2 ? (
+              <Image src="/img/crown.svg" alt="" className={styles.crown} />
+            ) : (
+              ''
+            )}
+            <ProfileCard key={item.id} src={item.src} alt="" text={item.name} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Ranking;
