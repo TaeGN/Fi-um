3cdf7ebf5a49ef57962473e254a6738b3efbb343
taegn <taegyunn123@gmail.com>
2023-09-14 14:00:17 +0900

    :black_nib: MOD / MainPage / mainPage 배경 색 수정

--- dev/FE/src/components/organisms/ranking/Ranking/Ranking.tsx
+++ dev/FE/src/components/organisms/ranking/Ranking/Ranking.tsx
@@ -1,15 +1,24 @@
 import { ProfileCard } from '@/components/molecules';
 import styles from './Ranking.module.scss';
 import { Image } from '@/components/atoms';
+import { convertClassName, convertClassNameList } from '@/utils';
 
-const Ranking = () => {
+interface RankingProps {
+  className?: string;
+}
+const Ranking = ({ className }: RankingProps): JSX.Element => {
   const dummy = [
     { id: 1, name: '1번사람', src: '/img/dummy2.jpg' },
     { id: 2, name: '2번사람', src: '/img/dummy2.jpg' },
     { id: 3, name: '3번사람', src: '/img/dummy2.jpg' },
   ];
   return (
-    <div className={styles.ranking}>
+    <div
+      className={convertClassNameList(
+        convertClassName(className, styles),
+        styles.ranking,
+      )}
+    >
       {dummy.map((item) => {
         return (
           <div className={styles[`profileCard${item.id}`]}>
