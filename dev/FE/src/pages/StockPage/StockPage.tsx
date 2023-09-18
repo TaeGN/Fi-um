098e7f45cc9c4a5a3fe532e140e8bd0e7358df5b
seungwookim <osabero@naver.com>
2023-09-18 10:59:04 +0900

    ADD/StockPage/StockPage api 연결 및 약간의 css 수정

--- dev/FE/src/pages/StockPage/StockPage.tsx
+++ dev/FE/src/pages/StockPage/StockPage.tsx
@@ -1,5 +1,40 @@
+import { getStockKing, getStocks } from '@/api/stock';
+import { Table } from '@/components/atoms';
+import { StockRanking } from '@/components/molecules';
+import { Stock, StockRanking as StockRank } from '@/types';
+import { useQuery } from '@tanstack/react-query';
+import styles from './StockPage.module.scss';
+
 const StockPage = () => {
-  return <div>StockPage</div>;
+  const { data: allStocks, status: isAllStocksLoading } = useQuery<
+    Stock[],
+    string
+  >(['getStockInformation'], getStocks);
+  console.log(allStocks, '전체 조회');
+  const { data: stockKing, status: isStockKingLoading } = useQuery<
+    StockRank[],
+    string
+  >(['getStockKing'], getStockKing);
+  console.log(stockKing, '랭킹조회');
+  return (
+    <div className={styles.stockPage}>
+      <div className={styles.allStocks}>
+        {isAllStocksLoading === 'success' ? (
+          <Table propsData={allStocks} />
+        ) : (
+          ''
+        )}
+      </div>
+      <div className={styles.newsWrapper}>뉴스 들어갈곳</div>
+      <div className={styles.rankingWrapper}>
+        {isStockKingLoading === 'success'
+          ? stockKing.map((item) => {
+              return <StockRanking key={item.userName} title={item.userName} />;
+            })
+          : ''}
+      </div>
+    </div>
+  );
 };
 
 export default StockPage;
