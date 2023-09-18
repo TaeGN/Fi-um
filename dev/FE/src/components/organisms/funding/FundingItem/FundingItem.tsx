8ec0ea5130c64687ba8c93d7f17bcfa2da4d32e3
신기정 <467shin@gmail.com>
2023-09-18 11:52:31 +0900

    MOD/fundingpage

--- dev/FE/src/components/organisms/funding/FundingItem/FundingItem.tsx
+++ dev/FE/src/components/organisms/funding/FundingItem/FundingItem.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import { convertClassName, convertClassNameList, loremData } from '@/utils';
 import styles from './FundingItem.module.scss';
 import { Image, Text } from '@/components/atoms';
 import { Funding } from '@/types';
-import { FundingBar } from '@/components/molecules';
+import { FundingBar, FundingDescription } from '@/components/molecules';
 
 interface FundingItemProps {
   className?: string;
@@ -10,37 +11,54 @@ interface FundingItemProps {
 }
 
 const FundingItem = ({ className, funding }: FundingItemProps): JSX.Element => {
+  const [showDescription, setShowDescription] = useState<boolean>(false);
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
+  };
+  console.log(funding);
+
   return (
-    <div
-      className={convertClassNameList(
-        convertClassName(className, styles),
-        styles['funding-item'],
-        'flex-container',
-        'container',
-      )}
-    >
-      <Image
-        className={convertClassNameList(styles['funding-item__image'])}
-        src={funding?.imagePath || '/vite.svg'}
-        alt={funding?.itemName}
-      />
-      <div className={convertClassNameList(styles['funding-item__main'])}>
-        <Text
-          className={convertClassNameList(
-            styles['funding-item__main'],
-            'text-xl',
-          )}
-          text={funding.itemName}
+    <div>
+      <div
+        className={convertClassNameList(
+          convertClassName(className, styles),
+          styles['funding-item'],
+          'flex-container',
+          'container',
+        )}
+        onClick={toggleDescription}
+      >
+        <Image
+          className={convertClassNameList(styles['funding-item__image'])}
+          src={funding?.imagePath || '/vite.svg'}
+          alt={funding?.itemName}
         />
-        <Text
-          className={convertClassNameList(
-            styles['funding-item__main'],
-            'text-xl',
-          )}
-          text={loremData}
-        />
-        <FundingBar ratio={80} />
+        <div className={convertClassNameList(styles['funding-item__main'])}>
+          <Text
+            className={convertClassNameList(
+              styles['funding-item__main'],
+              'text-xl',
+            )}
+            text={funding.itemName}
+          />
+          <Text
+            className={convertClassNameList(
+              styles['funding-item__main'],
+              'text-lg',
+            )}
+            text={loremData}
+          />
+          <FundingBar
+            ratio={
+              funding.fundingAmount !== 0
+                ? (funding.fundingAmount / funding.itemUnitPrice) * 100
+                : 0
+            }
+          />
+        </div>
       </div>
+      {showDescription ? <FundingDescription funding={funding} /> : <></>}
     </div>
   );
 };
