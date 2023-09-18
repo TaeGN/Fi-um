6849b7adcc33cf3a0ef304e9a110ac0452f1439c
김영우 <k950112@hanmail.net>
2023-09-18 13:35:24 +0900

    ADD / ImageUploadController / 이미지 업로드 로직

---
+++ dev/BE/src/main/java/com/example/pium/controller/ImageUploadController.java
@@ -0,0 +1,63 @@
+package com.example.pium.controller;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+@RestController
+public class ImageUploadController {
+
+    private static final String UPLOAD_DIR = "/app/images";
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            // 원래 파일의 확장자 가져오기
+            String originalFilename = file.getOriginalFilename();
+            String fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+            // UUID를 이용하여 고유한 파일명 생성
+            String uniqueFilename = UUID.randomUUID().toString() + fileExtension;
+
+            // 파일 저장 경로 설정
+            Path path = Paths.get(UPLOAD_DIR, uniqueFilename);
+
+            // 파일 저장
+            file.transferTo(new File(path.toString()));
+
+            String imageUrl = "/images/" + uniqueFilename;
+            return new ResponseEntity<>(imageUrl, HttpStatus.OK);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Failed to upload the file", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/images/{filename:.+}")
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Path file = Paths.get(UPLOAD_DIR).resolve(filename);
+        try {
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
+                return ResponseEntity.ok(resource);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+        } catch (MalformedURLException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+}
