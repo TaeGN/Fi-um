7a5c959348fce6cde59864807b2f5ef1d3611460
xjffla12 <xjffla12@naver.com>
2023-09-19 10:01:36 +0900

    ADD / Scheduler / 펀딩 랭킹 초기화

--- dev/BE/src/main/java/com/example/pium/repository/BalanceSheetRepository.java
+++ dev/BE/src/main/java/com/example/pium/repository/BalanceSheetRepository.java
@@ -1,6 +1,9 @@
 package com.example.pium.repository;
 
 import com.example.pium.dto.UserNoDto;
+import com.example.pium.dto.projection.AuctionRank;
+import com.example.pium.dto.projection.Rank;
+import com.example.pium.dto.projection.StockRank;
 import com.example.pium.entity.BalanceSheetEntity;
 import com.example.pium.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -22,4 +25,17 @@ public interface BalanceSheetRepository extends JpaRepository<BalanceSheetEntity
     @Query(value = "select b.user_no from balance_sheet as b where b.saving > 0", nativeQuery = true)
     List<Integer> haveSaving();
 
+    // 주식 수익 기준으로 상위 3명 , 동점자가 있는 경우 현재 주식 계좌에 돈이 많은 순으로 출력
+    @Query(value = "SELECT user_no as userNo,stock_income as price,RANK() OVER (ORDER BY stock_income DESC,stock DESC) AS ranking FROM balance_sheet limit 3", nativeQuery = true)
+    List<Rank> getStockRanking();
+
+    // 그림 판매 금액 순으로 상위 3명
+    @Query(value = "SELECT user_no as userNo, auction_income as price, RANK() OVER (ORDER BY auction_income DESC) AS ranking FROM balance_sheet limit 3",nativeQuery = true)
+    List<Rank> getAuctionRanking();
+
+    @Query(value = "SELECT user_no as userNo,donation as price, RANK() OVER (ORDER BY donation DESC) AS ranking FROM balance_sheet limit 3",nativeQuery = true)
+    List<Rank> getDonationRanking();
+
+    @Query(value = "select a.userNo as userNo, price, RANK() OVER (ORDER BY price DESC, count DESC) AS ranking   from (select u.user_no as userNo, price, count from user u ,(select user_no, sum(price) as price, count(*) as count from sponsor_funding_history as s group by user_no) as sfh where u.user_no = sfh.user_no and u.user_type = 2) as a",nativeQuery = true)
+    List<Rank> getFundingRanking();
 }
