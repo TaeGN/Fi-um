e3e50efbb1cfbcafa35b9fc45034a9a3127cc322
신기정 <467shin@gmail.com>
2023-09-26 17:34:41 +0900

    MOD/AuctionCard/텍스트 위치 등등

--- dev/FE/src/components/molecules/cards/AuctionCard/AuctionCard.tsx
+++ dev/FE/src/components/molecules/cards/AuctionCard/AuctionCard.tsx
@@ -33,28 +33,35 @@ const AuctionCard = ({
         <div className={styles.image}>
           <Image src={itemImagePath} alt={title} />
         </div>
+
         <div className={styles.title}>
           <Text text={title} className="text-lg" />
         </div>
         <div className={styles.content}>
           {auctionPrice && (
-            <Text text={`현재가 : ${priceFilter(auctionPrice)}`} />
+            <Text
+              className="bold"
+              text={`현재가 : ${priceFilter(auctionPrice)}`}
+            />
           )}
           {instantPrice && (
-            <Text text={`즉시구매가 : ${priceFilter(instantPrice)}`} />
+            <Text
+              className="bold"
+              text={`즉시구매가 : ${priceFilter(instantPrice)}`}
+            />
           )}
           {content && <Text text={content} />}
         </div>
-        {!noBtn && (
-          <div className={styles.button}>
-            <Button
-              label={review ? '자세히 보기' : '경매하러가기'}
-              className="primary xsmall"
-              onClick={onClick}
-            />
-          </div>
-        )}
       </div>
+      {!noBtn && (
+        <div className={styles.button}>
+          <Button
+            label={review ? '자세히 보기' : '경매하러가기'}
+            className="primary xsmall"
+            onClick={onClick}
+          />
+        </div>
+      )}
     </div>
   );
 };
