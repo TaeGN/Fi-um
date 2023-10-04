54022fda0510a5497fea5b23bedfa568cc320c1f
이승준 <ja132824@naver.com>
2023-10-04 16:21:26 +0900

    ADD / flask / ãrequirements 추가 및 데이터 수정

--- dev/Data/flask/app.py
+++ dev/Data/flask/app.py
@@ -6,14 +6,21 @@ from pypfopt.efficient_frontier import EfficientFrontier
 from pypfopt import risk_models, expected_returns
 from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
 import time
-
+from flask_cors import CORS
 
 app = Flask(__name__)
-start_time = 1695600000
-assets = ["공", "스피드킹", "마법쿠키", "해적선", "고양이"]
+CORS(app)
+start_time = 1696377600
+assets = [
+    "토이케라톱스 공룡장난감 주식회사",
+    "스피드킹 자동차 주식회사",
+    "마법의 쿠키 과자 주식회사",
+    "신비한 해적선 모험 주식회사",
+    "꿈꾸는 고양이 카페 주식회사",
+]
 
 
-def makeCountValance(userNo):
+def makeCountValance(userNo, df):
     conn = pymysql.connect(
         host="j9a308.p.ssafy.io",
         user="root",
@@ -21,7 +28,6 @@ def makeCountValance(userNo):
         db="backend",
         charset="utf8",
     )
-    df = pd.DataFrame()
     cur = conn.cursor()
     now_time = time.time()
     search_time = round(now_time - start_time / 60, 0)
@@ -35,7 +41,7 @@ def makeCountValance(userNo):
         prices = [row[0] for row in rows]
         df[assets[i - 1]] = prices
 
-    mu = expected_returns.mean_historical_return(df)
+    mu = expected_returns.mean_historical_return(df, frequency=52)
     S = risk_models.sample_cov(df)
     ef = EfficientFrontier(mu, S)
 
@@ -67,7 +73,6 @@ def makeCountValance(userNo):
         # 위험선호도에 맞는 포트폴리오 최적화 clean_weights를 활용하여 0에 가까운 값은 0으로 바꾸고 전체를 합쳐서 1이 되게끔 해줌.
         weights = ef.efficient_risk(target_volatility=risk_tolerance)
         cleaned_weights = ef.clean_weights()
-
         latest_prices = get_latest_prices(df)
         weights = cleaned_weights
         # 현재 내가 가지고 있는 돈까지 입력을 받아서 내게 맞는 포트폴리오 제작시 각 주식을 얼마나 사면 되는지 계산해주고 남는돈까지 계산
@@ -77,19 +82,19 @@ def makeCountValance(userNo):
         obj = {
             "ratioBalance": cleaned_weights,
             "moneyBalance": {
-                "공룡": int(allocation.get("공룡", 0)),
-                "스피드킹": int(allocation.get("스피드킹", 0)),
-                "마법쿠키": int(allocation.get("마법쿠키", 0)),
-                "해적선": int(allocation.get("해적선", 0)),
-                "고양이": int(allocation.get("고양이", 0)),
+                "토이케라톱스 공룡장난감 주식회사": int(allocation.get("토이케라톱스 공룡장난감 주식회사", 0)),
+                "스피드킹 자동차 주식회사": int(allocation.get("스피드킹 자동차 주식회사", 0)),
+                "마법의 쿠키 과자 주식회사": int(allocation.get("마법의 쿠키 과자 주식회사", 0)),
+                "신비한 해적선 모험 주식회사": int(allocation.get("신비한 해적선 모험 주식회사", 0)),
+                "꿈꾸는 고양이 카페 주식회사": int(allocation.get("꿈꾸는 고양이 카페 주식회사", 0)),
             },
         }
         return jsonify(obj)
     except Exception as e:
-        mu = expected_returns.mean_historical_return(df)
+        mu = expected_returns.mean_historical_return(df, frequency=52)
         S = risk_models.sample_cov(df)
-        ef = EfficientFrontier(mu, S)
-        weights = ef.max_sharpe()
+        ef = EfficientFrontier(mu, S, solver="COBYLA")
+        weights = ef.max_sharpe(risk_free_rate=0)
         cleaned_weights = ef.clean_weights()
 
         latest_prices = get_latest_prices(df)
@@ -101,11 +106,11 @@ def makeCountValance(userNo):
         obj = {
             "ratioBalance": cleaned_weights,
             "moneyBalance": {
-                "공룡": int(allocation.get("공룡", 0)),
-                "스피드킹": int(allocation.get("스피드킹", 0)),
-                "마법쿠키": int(allocation.get("마법쿠키", 0)),
-                "해적선": int(allocation.get("해적선", 0)),
-                "고양이": int(allocation.get("고양이", 0)),
+                "토이케라톱스 공룡장난감 주식회사": int(allocation.get("토이케라톱스 공룡장난감 주식회사", 0)),
+                "스피드킹 자동차 주식회사": int(allocation.get("스피드킹 자동차 주식회사", 0)),
+                "마법의 쿠키 과자 주식회사": int(allocation.get("마법의 쿠키 과자 주식회사", 0)),
+                "신비한 해적선 모험 주식회사": int(allocation.get("신비한 해적선 모험 주식회사", 0)),
+                "꿈꾸는 고양이 카페 주식회사": int(allocation.get("꿈꾸는 고양이 카페 주식회사", 0)),
             },
         }
         return jsonify(obj)
@@ -113,7 +118,8 @@ def makeCountValance(userNo):
 
 @app.route("/api/v1/portfolio/<int:userNo>")
 def makeCount(userNo):
-    return makeCountValance(userNo)
+    df = pd.DataFrame()
+    return makeCountValance(userNo, df)
 
 
 if __name__ == "__main__":
