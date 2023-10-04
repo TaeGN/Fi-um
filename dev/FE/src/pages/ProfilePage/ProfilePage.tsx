ad623e08aec1b6739e8e53d300773038e02d41b5
신기정 <467shin@gmail.com>
2023-10-04 16:55:50 +0900

    MOD/ProfilePage/내가 구매한 그림

--- dev/FE/src/pages/ProfilePage/ProfilePage.tsx
+++ dev/FE/src/pages/ProfilePage/ProfilePage.tsx
@@ -18,7 +18,7 @@ import {
 } from '@/utils';
 import { Image, Table, Text } from '@/components/atoms';
 import useAuth from '@/hooks/useAuth';
-import { useMemo, useState, useCallback, useEffect } from 'react';
+import { useMemo, useState, useCallback } from 'react';
 import { USER_TYPE } from '@/constants';
 import {
   ArtistAuction,
@@ -100,14 +100,16 @@ const SponsorProfilePage = () => {
   return (
     <>
       <ProfileSection label="내가 구매한 그림">
-        {auctionPurchases?.map(({ actionNo, imagePath, title }) => (
-          <AuctionCard
-            key={actionNo}
-            itemImagePath={imagePath}
-            title={title}
-            noBtn={true}
-          />
-        ))}
+        <div className="card-container jc-center">
+          {auctionPurchases?.map(({ actionNo, imagePath, title }) => (
+            <AuctionCard
+              key={actionNo}
+              itemImagePath={imagePath}
+              title={title}
+              noBtn={true}
+            />
+          ))}
+        </div>
       </ProfileSection>
       <ProfileSection label="팔로우 한 아이들">
         <Swiper>{followings}</Swiper>
@@ -184,12 +186,7 @@ const ChildProfilePage = ({ myPage }: { myPage?: boolean }) => {
       const len = myAuctions?.length ?? 0;
       for (let index = 0; index < len / 4; index++) {
         auction.push(
-          <div
-            className={convertClassNameList(
-              styles['profile-page__auction-container'],
-              'flex-container',
-            )}
-          >
+          <div className={styles['profile-page__auction-container']}>
             {myAuctions
               .slice(index * 4, Math.min((index + 1) * 4, len))
               .map(({ auctionNo, imagePath, title }) => (
@@ -292,10 +289,11 @@ const ChildProfilePage = ({ myPage }: { myPage?: boolean }) => {
 };
 
 const AdminProfilePage = () => {
-  const [curCapital, setCurCapital] = useState<TotalCapital>();
+  const [chartState, setChartState] = useState<number>(0);
 
-  const handleChangeChartState = useCallback((capital: TotalCapital) => {
-    setCurCapital(capital);
+  const handleChangeChartState = useCallback((userNo: number) => {
+    if (userNo === chartState) setChartState(0);
+    else setChartState(userNo);
   }, []);
 
   // 아이들 전체 자산
@@ -316,10 +314,6 @@ const AdminProfilePage = () => {
     getFundingRecordsQuery(),
   );
 
-  useEffect(() => {
-    totalCapitals?.[0] && setCurCapital(totalCapitals[0]);
-  }, [totalCapitals]);
-
   return (
     <>
       <div className={styles['profile-page__profile-section']}>
@@ -329,8 +323,8 @@ const AdminProfilePage = () => {
             onClick={handleChangeChartState}
           />
         </ProfileSection>
-        <ProfileSection label={`${curCapital?.userName}의 자산 상세`}>
-          <UserCapital totalCapitals={totalCapitals} curCapital={curCapital} />
+        <ProfileSection label="아이들 자산 상세">
+          <UserCapital totalCapitals={totalCapitals} chartState={chartState} />
         </ProfileSection>
       </div>
       <div className={styles['profile-page__profile-section2']}>
