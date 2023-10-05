e6a9800abc704af630f580f61a9131301192a4d2
Mac <467shin@gmail.com>
2023-10-06 02:09:57 +0900

    MOD/creator profile

--- dev/FE/src/components/organisms/user/CreatorProfile/CreatorProfile.tsx
+++ dev/FE/src/components/organisms/user/CreatorProfile/CreatorProfile.tsx
@@ -1,7 +1,7 @@
 import { convertClassName, convertClassNameList } from '@/utils';
 import styles from './CreatorProfile.module.scss';
-import { ProfileCard } from '@/components/molecules';
-import { Image, Text } from '@/components/atoms';
+import { AuctionCard, ProfileCard } from '@/components/molecules';
+import { Text } from '@/components/atoms';
 import { Art } from '@/types';
 import { useLocation } from 'react-router-dom';
 
@@ -23,30 +23,38 @@ const CreatorProfile = ({
         styles['creator-profile'],
       )}
     >
-      <ProfileCard
-        myPage={false}
-        src={state?.userImagePath}
-        alt={state?.name}
-        userNo={state?.userNo}
-      />
+      <div className="flex-container align-center">
+        <ProfileCard
+          myPage={false}
+          src={state?.userImagePath}
+          alt={state?.name}
+          userNo={state?.userNo}
+        />
+      </div>
       <div>
         <Text className="text-lg" text="이 작가의 다른 작품" />
         <div className={styles['creator-profile_arts']}>
           {arts?.map((art) => (
-            <div key={art.imagePath} className={styles['creator-profile_art']}>
-              <Image
-                src={art.imagePath}
-                className={
-                  art.winner
-                    ? styles['creator-profile_art__image__dark']
-                    : styles['creator-profile_art__image']
-                }
-              />
-              <div className="flex-container">
-                <Text text={`${art.title}`} />
-                {art.winner && <Text className="bold" text="(판매 완료)" />}
-              </div>
-            </div>
+            <>
+              <AuctionCard itemImagePath={art.imagePath} title={art.title} />
+              {/* <div
+                key={art.imagePath}
+                className={styles['creator-profile_art']}
+              >
+                <Image
+                  src={art.imagePath}
+                  className={
+                    art.winner
+                      ? styles['creator-profile_art__image__dark']
+                      : styles['creator-profile_art__image']
+                  }
+                />
+                <div className="flex-container">
+                  <Text text={`${art.title}`} />
+                  {art.winner && <Text className="bold" text="(판매 완료)" />}
+                </div>
+              </div> */}
+            </>
           ))}
         </div>
       </div>
